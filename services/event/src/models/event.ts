7bc7618acd5325de6fa49764cdae4b50cf7668e0
Aayush Seth <aayushseth8@gmail.com>
2022-02-09 13:22:06 -0500

    Create model with corresponding fields

--- services/event/src/models/event.ts
+++ services/event/src/models/event.ts
@@ -2,6 +2,16 @@ import { Schema, model } from "mongoose";
 
 interface Event {}
 
-const eventSchema = new Schema<Event>({});
+const eventSchema = new Schema<Event>({
+    name: { 
+        type: String,
+        required: true 
+    },
+    isActive: { 
+        type: Boolean, 
+        required: true,
+        default: true 
+    }
+});
 
 const EventModel = model<Event>("Event", eventSchema);
