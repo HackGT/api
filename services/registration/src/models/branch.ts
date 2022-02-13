97436482a73c64c1f222f3f1aeaefa7b3aa6fc02
dpopescu3 <91040119+dpopescu3@users.noreply.github.com>
2022-02-12 20:11:04 -0500

    mongoose model for a branch

--- services/registration/src/models/branch.ts
+++ services/registration/src/models/branch.ts
@@ -1,7 +1,18 @@
 import { Schema, model } from "mongoose";
 
-interface Branch {}
+interface Branch {
+    name: string;
+    type: string;
+    settings:{
+        open: Date,
+        close:Date;
+    }
+}
 
-const branchSchema = new Schema<Branch>({});
+const branchSchema = new Schema<Branch>({
+    name: { type: String, required: true },
+    type: { type: String, required: true },
+    settings:{type: String, required: true} 
+});
 
 const BranchModel = model<Branch>("Branch", branchSchema);
