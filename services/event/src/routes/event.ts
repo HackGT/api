1f3888f95d334a4e475068b7930c381eb523d836
Aayush Seth <aayushseth8@gmail.com>
2022-02-13 16:51:33 -0500

    Complete GET and POST routes

--- services/event/src/routes/event.ts
+++ services/event/src/routes/event.ts
@@ -1,17 +1,39 @@
 import { asyncHandler } from "@api/common";
 import express from "express";
+import { EventModel } from "../models/event";
 
 export const eventRouter = express.Router();
 
+/**
+ * Returns a list of all events (models)
+ */
 eventRouter.route("/").get(
   asyncHandler(async (req, res) => {
-    res.send();
+    const events = await EventModel.find();
+    // Get all the events (probably from Mongoose?)
+
+    res.status(200).send(events);
   })
 );
 
+/**
+ * Creates a new event (model) based on the POST body data
+ */
 eventRouter.route("/").post(
   asyncHandler(async (req, res) => {
-    res.send();
+    const { name, isActive } = req.body;
+
+    if (!req.body.name) {
+      res.status(400);
+      throw new Error("Please add a name field");
+    }
+
+    const createdEvent = await EventModel.create({
+      name,
+      isActive,
+    });
+
+    res.status(200).send(createdEvent);
   })
 );
 
