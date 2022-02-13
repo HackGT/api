8ae0e0962d3abb546b963475aaa60865083b8c91
Ayush Goyal <ayush-goyal@users.noreply.github.com>
2022-02-13 16:32:37 -0500

    Revert "Thaneesh"

--- services/checkin/src/models/event.ts
+++ services/checkin/src/models/event.ts
@@ -1,35 +1,7 @@
-import mongoose, { Schema, model } from "mongoose";
+import { Schema, model } from "mongoose";
 
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-interface RootDocument {
-  _id: mongoose.Types.ObjectId;
-}
-export function createNew<T extends RootDocument>(
-  model: mongoose.Model<T & mongoose.Document, {}>,
-  doc: Omit<T, "_id">
-) {
-  return new model(doc);
-}
+interface Checkin {}
 
-export interface Checkin extends RootDocument {
-  userId: string;
-  eventId: mongoose.Types.ObjectId;
-  status: string;
-}
+const checkinSchema = new Schema<Checkin>({});
 
-const checkinSchema = new Schema<Checkin & mongoose.Document>({
-  userId: {
-    type: String,
-    required: true
-  },
-  eventId: {
-    type: mongoose.Types.ObjectId,
-    required: false
-  },
-  status: {
-    type: String,
-    default: "NOT_CHECKED_IN"
-  }
-});
-
-export const CheckinModel = model<Checkin & mongoose.Document>("Checkin", checkinSchema);
+const CheckinModel = model<Checkin>("Checkin", checkinSchema);
--- services/checkin/src/routes/checkin.ts
+++ services/checkin/src/routes/checkin.ts
@@ -1,30 +1,17 @@
 import { asyncHandler } from "@api/common";
 import express from "express";
-import { createNew, Checkin, CheckinModel } from "../models/event"
 
 export const checkinRouter = express.Router();
 
 checkinRouter.route("/").get(
   asyncHandler(async (req, res) => {
-    const checkins = Array<string>();
-    const users = await CheckinModel.find({});
-    users.forEach((user) => { checkins.push(user.status) })
-
-    return res.send(checkins);
+    res.send();
   })
 );
 
 checkinRouter.route("/").post(
   asyncHandler(async (req, res) => {
-    let checkin = createNew<Checkin>(CheckinModel, {
-      userId: req.body.userId,
-      eventId: req.body.eventId,
-      status: req.body.status
-    });
-
-    await checkin.save();
-
-    return res.send({ id: checkin._id });
+    res.send();
   })
 );
 
