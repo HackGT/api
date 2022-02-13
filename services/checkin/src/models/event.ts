6bb2990bb3dadcaee56da3578c84e433e82b292f
Thaneesh Babu <63804742+thaneesh-babu@users.noreply.github.com>
2022-02-13 16:45:21 -0500

    created schema for checkin

--- services/checkin/src/models/event.ts
+++ services/checkin/src/models/event.ts
@@ -1,23 +1,12 @@
 import mongoose, { Schema, model } from "mongoose";
 
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-interface RootDocument {
-  _id: mongoose.Types.ObjectId;
-}
-export function createNew<T extends RootDocument>(
-  model: mongoose.Model<T & mongoose.Document, {}>,
-  doc: Omit<T, "_id">
-) {
-  return new model(doc);
-}
-
-export interface Checkin extends RootDocument {
+export interface Checkin {
   userId: string;
   eventId: mongoose.Types.ObjectId;
   status: string;
 }
 
-const checkinSchema = new Schema<Checkin & mongoose.Document>({
+const checkinSchema = new Schema<Checkin>({
   userId: {
     type: String,
     required: true
@@ -32,4 +21,4 @@ const checkinSchema = new Schema<Checkin & mongoose.Document>({
   }
 });
 
-export const CheckinModel = model<Checkin & mongoose.Document>("Checkin", checkinSchema);
+export const CheckinModel = model<Checkin>("Checkin", checkinSchema);
