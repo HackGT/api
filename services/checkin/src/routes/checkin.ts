a40017f035422b3e978354d17967b6cce2d48e07
Thaneesh Babu <63804742+thaneesh-babu@users.noreply.github.com>
2022-02-13 11:32:29 -0500

    filled get and post routes for checkin

--- services/checkin/src/routes/checkin.ts
+++ services/checkin/src/routes/checkin.ts
@@ -1,17 +1,30 @@
 import { asyncHandler } from "@api/common";
 import express from "express";
+import { createNew, Checkin, CheckinModel } from "../models/event"
 
 export const checkinRouter = express.Router();
 
 checkinRouter.route("/").get(
   asyncHandler(async (req, res) => {
-    res.send();
+    const checkins = Array<string>();
+    const users = await CheckinModel.find({});
+    users.forEach((user) => { checkins.push(user.status) })
+
+    return res.send(checkins);
   })
 );
 
 checkinRouter.route("/").post(
   asyncHandler(async (req, res) => {
-    res.send();
+    let checkin = createNew<Checkin>(CheckinModel, {
+      userId: req.body.userId,
+      eventId: req.body.eventId,
+      status: req.body.status
+    });
+
+    await checkin.save();
+
+    return res.send({ id: checkin._id });
   })
 );
 
