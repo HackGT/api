075a7b39ad9770ea0822ad9384a313aafecac093
Aayush Seth <aayushseth8@gmail.com>
2022-02-14 10:13:26 -0500

    Clean code

--- services/event/src/routes/event.ts
+++ services/event/src/routes/event.ts
@@ -4,35 +4,24 @@ import { EventModel } from "../models/event";
 
 export const eventRouter = express.Router();
 
-/**
- * Returns a list of all events (models)
- */
 eventRouter.route("/").get(
   asyncHandler(async (req, res) => {
     const events = await EventModel.find();
-    // Get all the events (probably from Mongoose?)
-
     res.status(200).send(events);
   })
 );
 
-/**
- * Creates a new event (model) based on the POST body data
- */
 eventRouter.route("/").post(
   asyncHandler(async (req, res) => {
     const { name, isActive } = req.body;
-
     if (!req.body.name) {
       res.status(400);
-      throw new Error("Please add a name field");
+      throw new Error("Please add all fields");
     }
-
     const createdEvent = await EventModel.create({
       name,
       isActive,
     });
-
     res.status(200).send(createdEvent);
   })
 );
