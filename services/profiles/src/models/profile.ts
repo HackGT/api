8f0cdebbd64d34ef07ef159f4fb1128a77cce7bd
Ayush Goyal <agoyal2001@gmail.com>
2022-02-15 02:04:06 -0500

    Fix up profile schema

--- services/profiles/src/models/profile.ts
+++ services/profiles/src/models/profile.ts
@@ -1,31 +1,35 @@
 import { Schema, model } from "mongoose";
 
 export interface Profile {
-    first: String,
-    middle: String,
-    last: String,
-    phoneNumber: String,
-    gender: String,
+  name: {
+    first: string;
+    middle: string;
+    last: string;
+  };
+  phoneNumber: string;
+  gender: string;
 }
 
 const profileSchema = new Schema<Profile>({
+  name: {
     first: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     middle: {
-        type: String,
+      type: String,
     },
     last: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    phoneNumber: {
-        type: String,
-    },
-    gender: {
-        type: String,
-    },
+  },
+  phoneNumber: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
 });
 
 export const ProfileModel = model<Profile>("Profile", profileSchema);
