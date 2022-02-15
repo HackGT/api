99a798e4d3953fba9a4b96391473e401678eeaa3
Ayush Goyal <agoyal2001@gmail.com>
2022-02-15 00:09:49 -0500

    Add better start script and concurrently messages

---
+++ index.ts
@@ -0,0 +1,9 @@
+import concurrently from "concurrently";
+
+concurrently([
+  { command: "cd gateway && yarn dev", name: "gateway" },
+  { command: "cd services/events && yarn dev", name: "events" },
+  { command: "cd services/profiles && yarn dev", name: "profiles" },
+  { command: "cd services/checkin && yarn dev", name: "checkin" },
+  { command: "cd services/registration && yarn dev", name: "registration" },
+]);
