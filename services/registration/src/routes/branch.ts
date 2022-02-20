05a5cf022cfb1ef9dc183dfc70246ed487ef5446
dpopescu3 <91040119+dpopescu3@users.noreply.github.com>
2022-02-20 15:55:56 -0500

    created the routes

--- services/registration/src/routes/branch.ts
+++ services/registration/src/routes/branch.ts
@@ -7,48 +7,36 @@ export const branchRouter = express.Router();
 
 branchRouter.route("/").get(
   asyncHandler(async (req, res) => {
-    const branches = await BranchModel.find();
-    res.send(branches);
+    const branches = await BranchModel.find({});
+    return res.send(branches);
   })
 );
 
 branchRouter.route("/").post(
   asyncHandler(async (req, res) => {
-    const new_branch = new BranchModel({
+    const new_branch = await BranchModel.create({
       name: req.body.name,
       type: req.body.type,
       settings: req.body.settings,
     });
-    await new_branch.save();
-    res.send(new_branch);
+    return res.send(new_branch);
   })
 );
 
 branchRouter.route("/:id").get(
   asyncHandler(async (req, res) => {
-    const new_branch = await BranchModel.findOne({ id: req.params.id });
-    res.send(new_branch);
+    const new_branch = await BranchModel.find({ _id: req.query.id });
+    return res.send(new_branch);
   })
 );
 
-branchRouter.route("/:id").put(
+branchRouter.route("/:id").patch(
   asyncHandler(async (req, res) => {
-    try {
-      const new_branch = await BranchModel.findOne({ id: req.params.id });
-      if (req.body.name) {
-        new_branch!.name = req.body.name;
-      }
-      if (req.body.type) {
-        new_branch!.type = req.body.type;
-      }
-      if (req.body.settings) {
-        new_branch!.settings = req.body.settings;
-      }
-      await new_branch!.save();
-      res.send(new_branch);
-    } catch {
-      res.status(400);
-      res.send({ error: "Branch doesn't exist!" });
-    }
+    const updated_branch = await BranchModel.findByIdAndUpdate(req.params.id, {
+      name: req.body.name,
+      type: req.body.type,
+      settings: req.body.settings,
+    });
+    return res.send(updated_branch);
   })
 );
