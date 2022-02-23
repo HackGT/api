42b56a2d3af1b571ae2e30998169764cd2f06010
Ayush Goyal <agoyal2001@gmail.com>
2022-02-22 20:04:15 -0500

    Change firebase env var to google

--- config/src/index.ts
+++ config/src/index.ts
@@ -30,9 +30,9 @@ if (COMMON.production) {
 } else {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: String(process.env.FIREBASE_PROJECT_ID),
-      clientEmail: String(process.env.FIREBASE_CLIENT_EMAIL),
-      privateKey: String(process.env.FIREBASE_PRIVATE_KEY).replace(/\\n/g, "\n"), // replace `\` and `n` character pairs w/ single `\n` character,
+      projectId: String(process.env.GOOGLE_PROJECT_ID),
+      clientEmail: String(process.env.GOOGLE_CLIENT_EMAIL),
+      privateKey: String(process.env.GOOGLE_PRIVATE_KEY).replace(/\\n/g, "\n"), // replace `\` and `n` character pairs w/ single `\n` character,
     }),
   });
 
