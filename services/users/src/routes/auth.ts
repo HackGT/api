87e3671d0ecce91e3bb9757e2e97ce341389a9b2
Ayush Goyal <agoyal2001@gmail.com>
2022-02-28 20:02:59 -0500

    Add domain for locahost dev

--- services/users/src/routes/auth.ts
+++ services/users/src/routes/auth.ts
@@ -39,6 +39,35 @@ authRoutes.route("/login").post(
   })
 );
 
+authRoutes.route("/login-localhost").post(
+  asyncHandler(async (req, res) => {
+    const { idToken } = req.body;
+
+    // Set session expiration to 5 days.
+    const expiresIn = 60 * 60 * 24 * 5 * 1000;
+
+    const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+
+    // Only process if the user just signed in in the last 5 minutes.
+    if (new Date().getTime() / 1000 - decodedIdToken.auth_time < 5 * 60) {
+      const sessionCookie = await admin.auth().createSessionCookie(idToken, { expiresIn });
+
+      // Set cookie policy for session cookie.
+      res.cookie("session", sessionCookie, {
+        maxAge: expiresIn,
+        httpOnly: true,
+        secure: config.common.production,
+        domain: "",
+      });
+      res.sendStatus(204);
+    } else {
+      // A user that was not recently signed in is trying to set a session cookie.
+      // To guard against ID token theft, require re-authentication.
+      throw new BadRequestError("Recent sign in is required");
+    }
+  })
+);
+
 authRoutes.route("/status").get(
   asyncHandler(async (req, res) => {
     const decodedIdToken = await admin.auth().verifySessionCookie(req.cookies.session || "");
