759553aff2fac668788ad1e31504d793f1f63bc1
Roshan Desai <rohod04@gmail.com>
2022-03-12 13:56:12 -0500

    fixed adding employees

--- services/users/src/routes/company.ts
+++ services/users/src/routes/company.ts
@@ -11,7 +11,6 @@ companyRoutes.route("/").post(
     const { name, defaultEmailDomains, hasResumeAccess, employees } = req.body;
 
     if (!name) {
-      res.status(400);
       throw new BadRequestError("Please enter the name field at the minimum");
     }
 
@@ -28,11 +27,10 @@ companyRoutes.route("/").post(
 
 companyRoutes.route("/:id").get(
   asyncHandler(async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const company = await CompanyModel.findById(id);
 
     if (!company) {
-      res.status(400);
       throw new BadRequestError("Company not found");
     }
     return res.status(200).send(company);
@@ -41,11 +39,10 @@ companyRoutes.route("/:id").get(
 
 companyRoutes.route("/:id").put(
   asyncHandler(async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const company = CompanyModel.findById(id);
 
     if (!company) {
-      res.status(400);
       throw new BadRequestError("Company not found");
     }
 
@@ -59,52 +56,46 @@ companyRoutes.route("/:id").put(
 
 companyRoutes.route("/:id/employees/add").post(
   asyncHandler(async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const company = await CompanyModel.findById(id);
 
     if (!company) {
-      res.status(400);
       throw new BadRequestError("Company not found");
     }
 
-    const emails = req.body.split(",");
+    const emails = req.body.employees.split(",");
     const newEmployees: string[] = [];
 
-    emails.forEach(async (index: number) => {
-      const email = emails[index];
-      const getUser = await getAuth().getUserByEmail(email);
-      const getUid = getUser.uid;
-      newEmployees.push(getUid);
-    });
-    // }) {
-    //     var email = emails[i];
-    //     var getUser = getAuth().getUserByEmail(email);
-    //     var getUid = (await getUser).uid
-    //     newEmployees.push(getUid);
-    // }
-
-    const addEmployees = await CompanyModel.findByIdAndUpdate(id, {
-      employees: newEmployees,
+    emails.forEach(async (email: string) => {
+      const user = await getAuth().getUserByEmail(email);
+      newEmployees.push(user.uid);
     });
 
-    return res.status(200).send(addEmployees);
+    const currEmployees = await CompanyModel.findByIdAndUpdate(
+      id,
+      {
+        employees: company.employees.concat(newEmployees),
+      },
+      { new: true }
+    );
+
+    return res.status(200).send(currEmployees);
   })
 );
 
 companyRoutes.route("/:id/employees").put(
   asyncHandler(async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const company = await CompanyModel.findById(id);
 
     if (!company) {
-      res.status(400);
       throw new BadRequestError("Company not found");
     }
 
     const addEmployees = await CompanyModel.findByIdAndUpdate(
       id,
       {
-        employees: req.body,
+        employees: req.body.employees,
       },
       { new: true }
     );
