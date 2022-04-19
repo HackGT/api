983b8ffb97350e6f65ab01e1f080aad325f959e0
Ayush Goyal <agoyal2001@gmail.com>
2022-04-19 17:56:16 -0400

    Update checkin and hexathon routes

--- services/hexathons/src/routes/hexathon.ts
+++ services/hexathons/src/routes/hexathon.ts
@@ -9,51 +9,38 @@ hexathonRouter.route("/").get(
   asyncHandler(async (req, res) => {
     const hexathons = await HexathonModel.find();
 
-    return res.status(200).send(hexathons);
+    return res.status(200).json(hexathons);
   })
 );
 
 hexathonRouter.route("/").post(
   asyncHandler(async (req, res) => {
-    const { name, isActive } = req.body;
-    if (!req.body.name) {
-      res.status(400);
-      throw new BadRequestError("Please add all fields");
-    }
-
-    const createdHexathon = await HexathonModel.create({
-      name,
-      isActive,
+    const newHexathon = await HexathonModel.create({
+      ...req.body,
     });
 
-    return res.status(200).send(createdHexathon);
+    return res.status(200).json(newHexathon);
   })
 );
 
 hexathonRouter.route("/:id").get(
   asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const hexathon = await HexathonModel.findById(id);
+    const hexathon = await HexathonModel.findById(req.params.id);
+
     if (!hexathon) {
-      res.status(400);
       throw new BadRequestError("Hexathon not found");
     }
+
     return res.status(200).send(hexathon);
   })
 );
 
 hexathonRouter.route("/:id").put(
   asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const hexathon = await HexathonModel.findById(id);
-    if (!hexathon) {
-      res.status(400);
-      throw new BadRequestError("Hexathon not found");
-    }
-
-    const updatedHexathon = await HexathonModel.findByIdAndUpdate(id, req.body, {
+    const updatedHexathon = await HexathonModel.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
     });
+
     res.status(200).json(updatedHexathon);
   })
 );
