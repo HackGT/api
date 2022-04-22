7101ffa3ae326cfe82780b343f9d320c82d3f468
Thaneesh Babu <63804742+thaneesh-babu@users.noreply.github.com>
2022-04-21 21:33:24 -0400

    changed queries for better efficiency

--- services/registration/src/routes/index.ts
+++ services/registration/src/routes/index.ts
@@ -1,56 +1,11 @@
-import { asyncHandler } from "@api/common";
 import express from "express";
 
-import { ApplicationModel } from "src/models/application";
-import { BranchModel, BranchType } from "src/models/branch";
 import { applicationRouter } from "./application";
 import { branchRouter } from "./branch";
+import { statisticsRouter } from "./statistics";
 
 export const defaultRouter = express.Router();
 
 defaultRouter.use("/branches", branchRouter);
 defaultRouter.use("/applications", applicationRouter);
-
-defaultRouter.route("/statistics").get(
-  asyncHandler(async (req, res) => {
-    const totalUsers = await ApplicationModel.countDocuments({});
-    const submittedUsers = await ApplicationModel.countDocuments({
-      applicationSubmitTime: { $exists: true, $ne: null },
-    });
-    const confirmedUsers = await ApplicationModel.countDocuments({
-      confirmationBranch: { $exists: true, $ne: null },
-    });
-    let applications = {};
-    await (
-      await BranchModel.find({ type: BranchType.APPLICATION })
-    ).forEach(async branch => {
-      const branchName = branch.name;
-      const count = await ApplicationModel.countDocuments({
-        applicationBranch: branch,
-      });
-      applications = { ...applications, [branchName]: count };
-    });
-
-    let confirmations = {};
-    await (
-      await BranchModel.find({ type: BranchType.CONFIRMATION })
-    ).forEach(async branch => {
-      const branchName = branch.name;
-      const count = await ApplicationModel.countDocuments({
-        confirmationBranch: branch,
-      });
-      confirmations = { ...confirmations, [branchName]: count };
-    });
-
-    const statistics = {
-      general: {
-        applicationStarted: totalUsers - submittedUsers,
-        applicationSubmitted: submittedUsers,
-        confirmed: confirmedUsers,
-      },
-      applications,
-      confirmations,
-    };
-    return res.send(statistics);
-  })
-);
+defaultRouter.use("/statistics", statisticsRouter);
