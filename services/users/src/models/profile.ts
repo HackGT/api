5818ef893d6148038aef60b9ebddd60a7e4f77e1
mxinburritos <mxinburritos@gmail.com>
2022-04-25 13:12:21 -0400

    Add new fields to profile schema

--- services/users/src/models/profile.ts
+++ services/users/src/models/profile.ts
@@ -2,11 +2,19 @@ import { Schema, model, Types } from "mongoose";
 
 export interface Profile {
   user: string;
+  job: string;
   name: {
     first: string;
     middle: string;
     last: string;
   };
+  school: {
+    name: string;
+    year: string;
+    major: string;
+  };
+  skills: string[];
+  bio: string;
   phoneNumber: string;
   gender: string;
   resume: Types.ObjectId;
@@ -30,6 +38,31 @@ const profileSchema = new Schema<Profile>({
       type: String,
       required: true,
     },
+  },
+  school: {
+    type: {
+      name: {
+        type: String,
+        required: false,
+      },
+      year: {
+        type: String,
+        required: false,
+      },
+      major: {
+        type: String,
+        required: false,
+      },
+    },
+    required: false,
+  },
+  skills: {
+    type: [String],
+    required: false,
+  },
+  bio: {
+    type: String,
+    required: false,
   },
   phoneNumber: {
     type: String,
