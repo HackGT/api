a5ffe0f1fc574f9211cdde39228d5eecd8e8788e
Ayush Goyal <agoyal2001@gmail.com>
2022-05-26 21:25:31 -0400

    Add comment for type

--- common/src/types.d.ts
+++ common/src/types.d.ts
@@ -1,3 +1,7 @@
 import mongoose from "mongoose";
 
+/**
+ * Follows the definition from mongoose.PopulatedDoc. Essentially a wrapper around a mongose
+ * type to include the _id field when populated.
+ */
 export type AutoPopulatedDoc<PopulatedType> = PopulatedType & { _id: mongoose.RefType };
