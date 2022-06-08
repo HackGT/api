648e1b7ce80b044f05b69054fbae3117451612ea
Ayush Goyal <agoyal2001@gmail.com>
2022-06-07 19:21:48 -0700

    Add shortCode field for a hexathon

--- services/hexathons/src/models/hexathon.ts
+++ services/hexathons/src/models/hexathon.ts
@@ -2,6 +2,7 @@ import { Schema, model } from "mongoose";
 
 export interface Hexathon {
   name: string;
+  shortCode: string;
   isActive: boolean;
   startDate: Date;
   endDate: Date;
@@ -11,6 +12,11 @@ const hexathonSchema = new Schema<Hexathon>({
   name: {
     type: String,
     required: true,
+  },
+  shortCode: {
+    type: String,
+    required: true,
+    unique: true,
   },
   isActive: {
     type: Boolean,
