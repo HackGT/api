b3ac2c583add0e29198ae055bf417fb17707c8dc
Thaneesh Babu <63804742+thaneesh-babu@users.noreply.github.com>
2022-07-09 14:57:35 -0400

    users stats route

--- services/registration/src/routes/stats.ts
+++ services/registration/src/routes/stats.ts
@@ -10,5 +10,29 @@ export const statisticsRouter = express.Router();
 statisticsRouter.route("/users").get(
   asyncHandler(async (req, res) => {
     // from the status field, get applied, accepted, confirmed and non-confirmed users
+    const aggregatedUsers = await ApplicationModel.aggregate([
+      {
+        $group: {
+          _id: "$status",
+          count: { $sum: 1 },
+        },
+      },
+    ]);
+
+    const draftUsers = aggregatedUsers.filter(application => application._id === "DRAFT")[0].count;
+    const appliedUsers = aggregatedUsers.filter(application => application._id === "APPLIED")[0]
+      .count;
+    const acceptedUsers = aggregatedUsers.filter(application => application._id === "ACCEPTED")[0]
+      .count;
+    const confirmedUsers = aggregatedUsers.filter(application => application._id === "CONFIRMED")[0]
+      .count;
+
+    const userStatistics = {
+      totalUsers: draftUsers + appliedUsers,
+      appliedUsers,
+      acceptedUsers,
+      confirmedUsers,
+      nonConfirmedUsers: acceptedUsers - confirmedUsers,
+    };
   })
 );
