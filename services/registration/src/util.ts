ad6689a37fccbf746b8d44c5b20efabe06bcb06a
Ayush Goyal <agoyal2001@gmail.com>
2022-07-20 02:32:55 -0700

    Update application validate to return errors

--- services/registration/src/util.ts
+++ services/registration/src/util.ts
@@ -45,5 +45,5 @@ export const validateApplicationData = async (
     return;
   }
 
-  throw new BadRequestError(`${validate.errors}`);
+  throw new BadRequestError(JSON.stringify(validate.errors, null, 4));
 };
