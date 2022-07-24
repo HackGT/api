cef580bd55c75202365bc147db5e3ffe1886bc79
Ayush Goyal <agoyal2001@gmail.com>
2022-07-24 01:53:18 -0700

    Remove auth routes from users

--- services/users/src/routes/index.ts
+++ services/users/src/routes/index.ts
@@ -1,13 +1,11 @@
 import express from "express";
 
-import { authRoutes } from "./auth";
 import { userRoutes } from "./user";
 import { teamRoutes } from "./team";
 import { companyRoutes } from "./company";
 
 export const defaultRouter = express.Router();
 
-defaultRouter.use("/auth", authRoutes);
 defaultRouter.use("/users", userRoutes);
 defaultRouter.use("/teams", teamRoutes);
 defaultRouter.use("/companies", companyRoutes);
