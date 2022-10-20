fdfea343c8d48e041c3ba11ff377be3e670e75bc
Ayush Goyal <agoyal2001@gmail.com>
2022-10-20 18:29:20 -0400

    Fix duplicate interactions by hexathon

--- services/hexathons/src/routes/interaction.ts
+++ services/hexathons/src/routes/interaction.ts
@@ -1,4 +1,4 @@
-import { asyncHandler, BadRequestError, checkAbility } from "@api/common";
+import { asyncHandler, BadRequestError, checkAbility, ConfigError } from "@api/common";
 import express from "express";
 import { FilterQuery } from "mongoose";
 
@@ -38,12 +38,29 @@ interactionRoutes.route("/").post(
     if ([InteractionType.EVENT, InteractionType.SCAVENGER_HUNT].includes(req.body.type)) {
       const existingInteraction = await InteractionModel.findOne({
         userId: req.body.userId,
+        type: req.body.type,
         identifier: req.body.identifier,
+        hexathon: req.body.hexathon,
       });
 
       if (existingInteraction) {
         throw new BadRequestError("Interaction already exists for this user and identifier");
       }
+    } else if (
+      // For these types, no identifier is required, but don't duplicate interactions
+      [InteractionType.CHECK_IN, InteractionType.EXPO_SUBMISSION].includes(req.body.type)
+    ) {
+      const existingInteraction = await InteractionModel.findOne({
+        userId: req.body.userId,
+        type: req.body.identifier,
+        hexathon: req.body.hexathon,
+      });
+
+      return res.send(existingInteraction);
+    } else {
+      throw new ConfigError(
+        "Interaction type must be setup on the backend. Please contact tech team."
+      );
     }
 
     const interaction = await InteractionModel.create({
