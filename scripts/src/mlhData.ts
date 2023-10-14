67b8c131ecdd299e4876ef825038809563c5386f
Roshan Desai <rohod04@gmail.com>
2023-10-14 13:54:32 -0400

    updated mlh data script for hackgt x

--- scripts/src/mlhData.ts
+++ scripts/src/mlhData.ts
@@ -18,35 +18,39 @@ const client = new MongoClient("mongodb://localhost:7777");
   const res = await collection
     .find({
       status: "CONFIRMED",
-      applicationBranch: {
+      confirmationBranch: {
         $in: [
-          new ObjectId("62d9f03418d8d494b683c316"),
-          new ObjectId("630a87ad4350de7de590ebd8"),
-          new ObjectId("630540a63d16456628e36b8a"),
-          new ObjectId("630540c23d16456628e36b8b"),
+          new ObjectId("64a35c5659be5f3bc28557e8"), // Participant Confirmation
+          new ObjectId("6529a20c0b9949f09de6e940"), // Participant Confirmation [Walk Up]
+          new ObjectId("64f370c0dc5faf15feec8ed5"), // Accepted - Participant [Flight/Gas]
+          new ObjectId("64f370ecdc5faf15feec8ef9"), // Accepted - Participant [Gas]
+          new ObjectId("64f37153dc5faf15feec8f48"), // Accepted - Participant [Flight I]
+          new ObjectId("64f3717cdc5faf15feec8f8b"), // Accepted - Participant [Flight II]
+          new ObjectId("64f37199dc5faf15feec8f9d"), // Accepted - Participant [Flight III]
         ],
       },
     })
     .toArray();
 
+  console.log(`Found ${res.length} users.`);
+
   const data = res.map(doc => ({
-    "name": doc.name,
-    "email": doc.email,
-    "dateOfBirth": doc.applicationData.dateOfBirth,
-    "school": doc.applicationData.school,
-    "phoneNumber": doc.applicationData.phoneNumber,
-    "country": doc.applicationData.countryOfResidence || "United States of America",
-    "levelOfStudy": doc.applicationData.levelOfStudy || "Undergraduate University (3+ year)",
-    "MLH-Code-Of-Conduct": doc.applicationData.confirmChecks["MLH-Code-Of-Conduct"] || false,
-    "MLH-Terms-and-Conditions":
-      doc.applicationData.confirmChecks["MLH-Terms-and-Conditions"] || false,
-    "MLH-Information-Emails": doc.applicationData.confirmChecks["MLH-Information-Emails"] || false,
+    name: doc.name,
+    email: doc.email,
+    dateOfBirth: doc.applicationData.dateOfBirth,
+    school: doc.applicationData.school,
+    phoneNumber: doc.applicationData.phoneNumber,
+    country: doc.applicationData.countryOfResidence || "United States of America",
+    levelOfStudy: doc.applicationData.levelOfStudy || "Undergraduate University (3+ year)",
+    // "MLH-Code-Of-Conduct": doc.applicationData.confirmChecks["MLH-Code-Of-Conduct"] || false,
+    // "MLH-Terms-and-Conditions":
+    //   doc.applicationData.confirmChecks["MLH-Terms-and-Conditions"] || false,
+    // "MLH-Information-Emails": doc.applicationData.confirmChecks["MLH-Information-Emails"] || false,
   }));
 
-  let output =
-    "Name,Email,Date of Birth,School,Phone Number,Country,Level of Study,MLH Code of Conduct,MLH Terms and Conditions,MLH Information Emails\n";
+  let output = "Name,Email,Date of Birth,School,Phone Number,Country,Level of Study\n"; // ,MLH Code of Conduct,MLH Terms and Conditions,MLH Information Emails\n
   for (const doc of data) {
-    output += `${doc.name},${doc.email},${doc.dateOfBirth},${doc.school},${doc.phoneNumber},${doc.country},${doc.levelOfStudy},${doc["MLH-Code-Of-Conduct"]},${doc["MLH-Terms-and-Conditions"]},${doc["MLH-Information-Emails"]}\n`;
+    output += `${doc.name},${doc.email},${doc.dateOfBirth},${doc.school},${doc.phoneNumber},${doc.country},${doc.levelOfStudy}\n`; // ,${doc["MLH-Code-Of-Conduct"]},${doc["MLH-Terms-and-Conditions"]},${doc["MLH-Information-Emails"]}\n
   }
 
   fs.mkdir(path.resolve(__dirname, "../output"), { recursive: true }, err => {
