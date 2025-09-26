1d50ff08fe7f9b6ada98ca7cc535be3e06c1d6ea
Lauren Ji <lyangji1011@gmail.com>
2025-09-26 11:20:25 -0400

    update comment

--- services/expo/src/utils/validationHelpers.ts
+++ services/expo/src/utils/validationHelpers.ts
@@ -320,7 +320,7 @@ export const validateTeam = async (members: any[], req: express.Request) => {
         return "";
       }
 
-      // Checks the user's applications and if they are confirmed for the current hexathon
+      // Checks the user's applications and if they are checked in for the current hexathon
       if (userApplication.status !== "CHECKED_IN") {
         registrationError = {
           error: true,
