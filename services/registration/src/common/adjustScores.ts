70d5a448926eca39a46d84a4b38322e0018f24a0
crystaltine <msh379c@outlook.com>
2026-08-21 15:39:00 -0700

    update referral bonus to 100 to not pollute true scoring

--- services/registration/src/common/adjustScores.ts
+++ services/registration/src/common/adjustScores.ts
@@ -26,7 +26,10 @@ type CriteriaScores = {
   score: number;
 }[];
 
-export const REFERRAL_BONUS = 2;
+// distinguishable referral bonus (points should never be this high for any other reason)
+// can we make a separate field for this? yes, however that requires handling a bunch of
+// edge cases so surely its okay to be lazy for now
+export const REFERRAL_BONUS = 100;
 
 /** The minimum grading score an essay can get. */
 const MIN_GRADING_SCORE = 1;
